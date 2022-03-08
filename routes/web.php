9f21d6dbd19fae49d3203acec67f91d8e99d6b70
cristiane <cristiane.motin@rpinfo.com.br>
2022-03-08 11:46:32 -0300

    menu dash

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlanController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+/* Route::get('admin/plans', 'PlanController@index')->name('plans.index'); */
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
+
+
 
 Route::get('/', function () {
     return view('welcome');
